18132bd0c0e208edc6cc711955e35a1959cba4f1
bhuztez <bhuztez@gmail.com>
2020-04-26 22:21:35 +0800

    grcov

--- c.py
+++ c.py
@@ -42,8 +42,8 @@ def libname(path):
 
 def cargo_argv(mode, target):
     yield 'cargo'
-    if mode == 'coverage':
-        yield 'cov'
+    # if mode == 'coverage':
+    #     yield 'cov'
     yield 'build'
     yield '--lib'
     yield '-v' if VERBOSE else '-q'
@@ -51,15 +51,12 @@ def cargo_argv(mode, target):
         yield '--release'
     if target is not None:
         yield from ('--target', target)
+    yield from ('--target-dir', os.path.dirname(os.path.join(ROOTDIR, *target_dir(mode, target))))
     yield from ('--features', ",".join(features(mode, target)))
     yield from ("--message-format", "json")
 
 def rustc_argv(mode, target, filename, *libs):
-    if mode == 'coverage':
-        yield os.path.join(ROOTDIR, 'target/cov/build/rustc-shim.bat')
-    else:
-        yield 'rustc'
-    yield from ('--edition', '2018')
+    yield from ('rustc', '--edition', '2018')
 
     if VERBOSE:
         yield '-v'
@@ -72,13 +69,20 @@ def rustc_argv(mode, target, filename, *libs):
         yield from ("-C", "lto=fat")
         yield from ("-C", "opt-level=2")
         yield from ("-C", "panic=abort")
+    if mode == 'coverage':
+        yield from ("-Z", "profile")
+        yield from ("-Z", "no-landing-pads")
+        yield from ("-C", "codegen-units=1")
+        yield from ("-C", "opt-level=0")
+        yield from ("-C", "link-dead-code")
+        yield from ("-C", "overflow-checks=off")
 
     if target is not None:
         yield from ('--target', target)
     for feature in features(mode, target):
         yield from ('--cfg', f'feature="{feature}"')
 
-    if mode != 'release':
+    if mode == 'debug':
         yield from ('-C', 'incremental='+os.path.join(ROOTDIR, *target_dir(mode, target), "incremental"))
     yield from ('-L', 'dependency='+os.path.join(ROOTDIR, *target_dir(mode, None), "deps"))
 
@@ -108,14 +112,17 @@ def lru1(func):
 def compile_libs(mode='debug', target=None):
     from subprocess import DEVNULL
     from wronganswer.subprocess import run
-    output = run(list(cargo_argv(mode, target)), stdin=DEVNULL, cwd=ROOTDIR, capture_output=True, check=True).stdout
+    env = os.environ.copy()
+    if mode == 'coverage':
+        env["CARGO_INCREMENTAL"] = "0"
+        env["RUSTFLAGS"] = "-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zno-landing-pads"
+    output = run(list(cargo_argv(mode, target)), stdin=DEVNULL, cwd=ROOTDIR, env=env, capture_output=True, check=True).stdout
     packages = [json.loads(line) for line in output.splitlines()]
 
-    return [ filename
+    return [ package["filenames"][0]
              for package in packages
              if package["reason"] == "compiler-artifact"
-             if "porus" in package["target"]["name"]
-             for filename in package["filenames"]]
+             if "porus" in package["target"]["name"]]
 
 # mode=coverage, target=None, run coverage locally
 # mode=debug, target=None, to run locally
@@ -128,10 +135,6 @@ def get_compile_argv(filename, *libs, mode='debug', target=None):
 
     if mode == 'coverage':
         env["CARGO_INCREMENTAL"] = "0"
-        env["COV_PROFILER_LIB_NAME"] = "@native"
-        env["COV_PROFILER_LIB_PATH"] = "@native"
-        env["COV_RUSTC"] = "rustc"
-        env["COV_BUILD_PATH"] = os.path.dirname(dest)
 
     return dest, list(rustc_argv(mode, target, filename, *libs)), env
 
